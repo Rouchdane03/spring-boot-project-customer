b27c46aa1236cc67fdd59a26001bbd9602de1dcb
Rdiss03 <moudjibourouchdan80@gmail.com>
2024-11-17 23:12:16 +0100

    refacto du pom plus suppresion liste des beans

--- src/main/java/com/rouchFirstCode/Main.java
+++ src/main/java/com/rouchFirstCode/Main.java
@@ -16,7 +16,7 @@ import java.time.Period;
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class,args);
-        printBeans(applicationContext);
+        //printBeans(applicationContext);
 }
 
 @Bean
